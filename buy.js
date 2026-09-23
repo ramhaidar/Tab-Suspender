c75e5d013816d8dad139d6e38aaa1e5e380fb9da
ramhaidar <ramhaidar@users.noreply.github.com>
2026-09-23 17:04:04 +0700

    refactor(payments): clean up buy shim
    - Replace function expressions with arrow callbacks and remove dead deobfuscation scaffolding in the buy shim.
    - Target: payments

--- buy.js
+++ buy.js
@@ -1,36 +1,42 @@
 (function () {
-	var f = this,
-		g = function (a, d) {
-			var c = a.split('.'),
-				b = window || f;
-			c[0] in b || !b.execScript || b.execScript('var ' + c[0]);
-			for (var e; c.length && (e = c.shift()); ) c.length || void 0 === d ? (b = b[e] ? b[e] : (b[e] = {})) : (b[e] = d);
-		};
-	var h = function (a) {
+	var g = (a, d) => {
+		var c = a.split('.'),
+			b = window || this;
+		c[0] in b || !b.execScript || b.execScript(`var ${c[0]}`);
+		var e;
+		while (c.length) {
+			e = c.shift();
+			if (c.length || void 0 === d) {
+				if (b[e] == null) b[e] = {};
+				b = b[e];
+			} else b[e] = d;
+		}
+	};
+	var h = (a) => {
 		var d = chrome.runtime.connect('nmmhkkegccagdldgiimedpiccmgmieda', {}),
 			c = !1;
-		d.onMessage.addListener(function (b) {
+		d.onMessage.addListener((b) => {
 			c = !0;
-			'response' in b && !('errorType' in b.response) ? a.success && a.success(b) : a.failure && a.failure(b);
+			'response' in b && !('errorType' in b.response) ? a.success?.(b) : a.failure?.(b);
 		});
-		d.onDisconnect.addListener(function () {
+		d.onDisconnect.addListener(() => {
 			!c && a.failure && a.failure({ request: {}, response: { errorType: 'INTERNAL_SERVER_ERROR' } });
 		});
 		d.postMessage(a);
 	};
-	g('google.payments.inapp.buy', function (a) {
+	g('google.payments.inapp.buy', (a) => {
 		a.method = 'buy';
 		h(a);
 	});
-	g('google.payments.inapp.consumePurchase', function (a) {
+	g('google.payments.inapp.consumePurchase', (a) => {
 		a.method = 'consumePurchase';
 		h(a);
 	});
-	g('google.payments.inapp.getPurchases', function (a) {
+	g('google.payments.inapp.getPurchases', (a) => {
 		a.method = 'getPurchases';
 		h(a);
 	});
-	g('google.payments.inapp.getSkuDetails', function (a) {
+	g('google.payments.inapp.getSkuDetails', (a) => {
 		a.method = 'getSkuDetails';
 		h(a);
 	});
